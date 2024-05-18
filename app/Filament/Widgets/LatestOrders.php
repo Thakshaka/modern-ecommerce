ee4d2c4bea518ac028656d3c5d4748a11f18a875
Hemini <heminigallella03@gmail.com>
2024-05-19 01:13:35 +0530

    Did some changes

---
+++ app/Filament/Widgets/LatestOrders.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Filament\Resources\OrderResource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Tables\Column\TextColumn;
+use Filament\Widgets\TableWidget as BaseWidget;
+
+class LatestOrders extends BaseWidget
+{
+    protected int | string | array $columnSpan = 'full';
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(OrderResource::getEloquentQuery())
+            ->defaultPaginationPageOption(5)
+            ->defaultSort('created_at', 'desc')
+            ->columns([
+                TextColumn::make('id')
+                    ->label('Order ID')
+                    ->searchable(),
+
+                TextColumn::make('user.name')
+                    ->searchable(),
+                
+
+                TextColumn::make('grand_total')
+                    ->money('LKR'),
+
+                TextColumn::make ('status')
+                    ->badge()
+                    ->color(fn (string $state): string => match ($state){
+                                'new' => 'New',
+                                'processing' => 'Processing',
+                                'shipped' => 'Shipped',
+                                'delivered' => 'Delivered',
+                                'cancelled' => 'Cancelled'
+                    })
+
+                    ->icon (fn (string $state): string => match ($state){
+                        'new' => 'heroicon-o-sparkles',
+                        'processing' => 'heroicon-o-arrow-path',
+                        'shipped' => 'heroicon-o-truck',
+                        'delivered' => 'heroicon-o-check-circle',
+                        'cancelled' => 'heroicon-o-x-circle'
+                    })
+                    ->sortable(),
+
+                TextColumn::make('payment_method')
+                    ->sortable()
+                    ->searchable(),
+                
+                TextColumn::make('payment_status')
+                    ->sortable()
+                    ->badge()
+                    ->searchable(),
+
+                TextColumn::make('created_at')
+                    ->label('Order Date')
+                    ->dateTime(),
+            ]);
+    }
+}
