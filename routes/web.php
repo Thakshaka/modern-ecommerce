08126076989f8c7415c00a8fd58d5c94673206a8
Thakshaka Rathnayake <thakshakarathnayake15@gmail.com>
2024-05-23 19:28:45 +0530

    Started creating enduser panel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Livewire\CategoriesPage;
+use App\Livewire\HomePage;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomePage::class);
+Route::get('/categories', CategoriesPage::class);
