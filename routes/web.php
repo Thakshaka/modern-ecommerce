b202877111a96ba4fe14afd915a4b1b474a874ec
Thakshaka Rathnayake <thakshakarathnayake15@gmail.com>
2024-05-27 23:21:05 +0530

    Created static pages, made nav bar dynamic, added wire navigation, made home page brands dynamic

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,19 @@
 <?php
 
+use App\Livewire\Auth\ForgotPage;
+use App\Livewire\Auth\LoginPage;
+use App\Livewire\Auth\RegisterPage;
+use App\Livewire\Auth\ResetPasswordPage;
+use App\Livewire\CancelPage;
 use App\Livewire\CartPage;
 use App\Livewire\CategoriesPage;
+use App\Livewire\CheckoutPage;
 use App\Livewire\HomePage;
+use App\Livewire\MyOrderDetailPage;
+use App\Livewire\MyOrdersPage;
 use App\Livewire\ProductDetailPage;
 use App\Livewire\ProductsPage;
+use App\Livewire\SuccessPage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomePage::class);
@@ -12,3 +21,12 @@ Route::get('/categories', CategoriesPage::class);
 Route::get('/products', ProductsPage::class);
 Route::get('/cart', CartPage::class);
 Route::get('/products/{product}', ProductDetailPage::class);
+Route::get('/checkout', CheckoutPage::class);
+Route::get('/my-orders', MyOrdersPage::class);
+Route::get('/my-orders/{order}', MyOrderDetailPage::class);
+Route::get('/login', LoginPage::class);
+Route::get('/forgot', ForgotPage::class);
+Route::get('/register', RegisterPage::class);
+Route::get('/reset', ResetPasswordPage::class);
+Route::get('/success', SuccessPage::class);
+Route::get('/cancel', CancelPage::class);
