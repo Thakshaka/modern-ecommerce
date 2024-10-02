c4810873a7443348dd847327f235223bdb1aa053
Thakshaka Rathnayake <thakshakarathnayake15@gmail.com>
2024-10-02 11:27:52 +0530

    changed colors

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -1,16 +1,16 @@
 <div>
     <!-- Hero Section Start -->
-    <div class="w-full h-screen bg-gradient-to-r from-blue-200 to-cyan-200 py-10 px-4 sm:px-6 lg:px-8 mx-auto">
+    <div class="w-full h-screen bg-gradient-to-r from-pink-100 to-pink-100 py-10 px-4 sm:px-6 lg:px-8 mx-auto">
   <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8">
     <!-- Grid -->
     <div class="grid md:grid-cols-2 gap-4 md:gap-8 xl:gap-20 md:items-center">
       <div>
-        <h1 class="block text-3xl font-bold text-gray-800 sm:text-4xl lg:text-6xl lg:leading-tight dark:text-white">Start your journey with <span class="text-blue-600">Cosmetics</span></h1>
-        <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">Purchase wide varities of cosmetics categories like Makeup, Skin Care and many more.</p>
+      <h1 class="block text-3xl font-bold text-gray-800 sm:text-4xl lg:text-6xl lg:leading-tight dark:text-white">Start your journey with <span class="text-rose-950">Cosmetics</span></h1>
+      <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">Purchase wide varities of Cosmetics categories like Makeup, Skin Care and many more.</p>
 
         <!-- Buttons -->
         <div class="mt-7 grid gap-3 w-full sm:inline-flex">
-          <a class="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="/register">
+        <a class="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-rose-950 text-white hover:bg-MediumVioletRed-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="/register">
             Get started
             <svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
               <path d="m9 18 6-6-6-6" />
@@ -147,14 +147,14 @@
   <div class="max-w-xl mx-auto">
     <div class="text-center ">
       <div class="relative flex flex-col items-center">
-        <h1 class="text-5xl font-bold dark:text-gray-200"> Browse Popular<span class="text-blue-500"> Brands
+        <h1 class="text-5xl font-bold dark:text-pink-200"> Browse Popular<span class="text-pink-950"> Brands
           </span> </h1>
         <div class="flex w-40 mt-2 mb-6 overflow-hidden rounded">
-          <div class="flex-1 h-2 bg-blue-200">
+          <div class="flex-1 h-2 bg-pink-300">
           </div>
-          <div class="flex-1 h-2 bg-blue-400">
+          <div class="flex-1 h-2 bg-pink-400">
           </div>
-          <div class="flex-1 h-2 bg-blue-600">
+          <div class="flex-1 h-2 bg-pink-500">
           </div>
         </div>
       </div>
@@ -187,18 +187,18 @@
     <!-- Brand Section End -->
 
     <!-- Categories Section Start -->
-    <div class="bg-orange-200 py-20">
+    <div class="bg-pink-100 py-20">
   <div class="max-w-xl mx-auto">
     <div class="text-center ">
       <div class="relative flex flex-col items-center">
-        <h1 class="text-5xl font-bold dark:text-gray-200"> Browse <span class="text-blue-500"> Categories
+        <h1 class="text-5xl font-bold dark:text-gray-200"> Browse <span class="text-pink-950"> Categories
           </span> </h1>
         <div class="flex w-40 mt-2 mb-6 overflow-hidden rounded">
-          <div class="flex-1 h-2 bg-blue-200">
+          <div class="flex-1 h-2 bg-pink-200">
           </div>
-          <div class="flex-1 h-2 bg-blue-400">
+          <div class="flex-1 h-2 bg-pink-300">
           </div>
-          <div class="flex-1 h-2 bg-blue-600">
+          <div class="flex-1 h-2 bg-pink-400">
           </div>
         </div>
       </div>
@@ -242,7 +242,7 @@
     <!-- Categories Section End -->
 
     <!-- Customer Reviews Section Start -->
-    <section class="py-14 font-poppins dark:bg-gray-800">
+    <!-- <section class="py-14 font-poppins dark:bg-gray-800">
   <div class="max-w-6xl px-4 py-6 mx-auto lg:py-4 md:px-6">
     <div class="max-w-xl mx-auto">
       <div class="text-center ">
@@ -587,6 +587,6 @@
       </div>
     </div>
   </div>
-</section>
+</section> -->
     <!-- Customer Reviews Section End -->
 </div>
